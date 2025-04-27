6ef93cc202da51cba5f343126ed7ae4f21f97de4
giayyuek <is.gi.yue@gmail.com>
2025-04-27 14:16:31 +0200

    Changes to be committed:       modified:   a.out       modified:   lilo.c
    Changes not staged for commit:
          deleted:    test.txt
    
        now it workes...

--- lilo.c
+++ lilo.c
@@ -1,54 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// TODO: structs, global variables, etc.
+/* ------------------------------------
+ * i hope this is sufficently commented
+ * ------------------------------------*/
 typedef struct Node {
   int value;
   struct Node *next;
 } node_t;
 
+// function signatures 
 static int insertElement(int value);
 static int removeElement(void);
-static node_t *head = NULL;
+int freeAll(void); // this is not asked of by the assignment 
+
+static node_t *head = NULL; // head of list
 
 int main (int argc, char* argv[]) {
-	printf("insert 47: %d\n", insertElement(23));
+	printf("insert 43: %d\n", insertElement(43));
+	printf("insert 43: %d\n", insertElement(43));
 	printf("insert 11: %d\n", insertElement(11));
-	printf("insert 23: %d\n", insertElement(23));
-	printf("insert 11: %d\n", insertElement(11));
+	printf("insert 11: %d\n", insertElement(11)); // value already inserted
 	printf("remove: %d\n", removeElement());
 	printf("remove: %d\n", removeElement());
 	printf("insert 23: %d\n", insertElement(23));
-	printf("remove: %d\n", removeElement());
-	printf("remove: %d\n", removeElement());
-	printf("remove: %d\n", removeElement());
 	printf("remove: %d\n", removeElement());
 	printf("remove: %d\n", removeElement());
 	printf("insert 23: %d\n", insertElement(23));
 	printf("insert 47: %d\n", insertElement(47));
+	printf("insert why? 47: %d\n", insertElement(47)); // value already inserted
 	printf("remove: %d\n", removeElement());
-	printf("insert -32: %d\n", insertElement(-32));
-	printf("remove: %d\n", removeElement());
-	printf("remove: %d\n", removeElement());
-	printf("remove: %d\n", removeElement());
+	printf("insert -32: %d\n", insertElement(-32)); // negative value test
 	printf("remove: %d\n", removeElement());
 	printf("remove: %d\n", removeElement());
-
-	// TODO: add more tests
-
+	printf("remove: %d\n", removeElement());
+	printf("remove: %d\n", removeElement());
+	printf("remove: %d\n", removeElement()); // removing from empty list
+	printf("insert 42: %d\n", insertElement(42));
+  // freeAll(); if list still has elements this function removes them
 	exit(EXIT_SUCCESS);
 }
 static int insertElement(int value) {
-  // we could recurs if malloc failes (could lead to stackoverflow).
-  // we interpete the problem spesification that -1 sould be returned.
-  if (head == NULL) {
-    head = (node_t*) malloc(sizeof(node_t));
-    return -1;
-  }
   // test to not insert a negative value
   if (value < 0) {
     return -1;
   }
+  if (head == NULL) { // if list is empty then create head object
+    head = (node_t*) malloc(sizeof(node_t));
+    head->value = value;
+    head->next = NULL;
+    return value;
+  }
+  // if list is not empty
   // head->next would destroy the list
   // we create *temp and and go to the last element if the values are diffrent 
   node_t *temp = head;
@@ -60,6 +63,7 @@ static int insertElement(int value) {
       return -1;
     }
   }
+  if (temp->value == value) {return -1;}
   // when the end of the list is reached we allocate the new node and insert it at the end;
   node_t *new = (node_t*)malloc(sizeof(node_t));
   if (new == NULL) {
@@ -72,23 +76,35 @@ static int insertElement(int value) {
 }
 
 static int removeElement(void) {
-  // go to end of list using head would destroy the list
-  if (head == NULL) {
+  if (head == NULL) { // if list is empty
     return -1;
   }
-  if (head->next == NULL) {
-    return -1;
+  else if (head->next == NULL) { // else if list has 1 element
+    int ret = head->value;
+    free(head);
+    head = NULL;
+    return ret;
   }
-  node_t *temp = head;
-  while(temp->next != NULL) {
-    if(temp->next->next == NULL) {
-      break;
-    }
-    temp = temp->next;
+  else {
+    node_t *newHead = head->next; // if list has more then 1 element 
+    int ret = head->value;
+    head = NULL;
+    head = newHead;
+    return ret;
   }
-  int ret = temp->next->value;
-  free(temp->next);
-  temp->next = NULL;
-	return ret;
 }
 
+int freeAll() { 
+  // normaly you would have to free the entire rest of the list
+  // at the end of the program.
+  // this is not specified in the assignment. 
+  // it could look like this.
+  if (head) {
+    while (head->next != NULL) {
+      removeElement();
+    }
+    free(head);
+    head = NULL;
+  }
+  return 0;
+}
